d3c06758d5ea4dda36c7d7b021a456cadab3f5fa
Therarat <117662533+Therarat@users.noreply.github.com>
2022-11-08 22:14:03 +0700

    Update Linear_regression.r

--- Machine_learning/Linear_regression.r
+++ Machine_learning/Linear_regression.r
@@ -1,10 +1,7 @@
-set.seed(99)
-ctrl <- trainControl(method = “cv”, number = 5, 
- verboseIter = TRUE)
-logistic_model <- train(diabetes ~ ., 
- data = train_data, 
- method = "glm",
- trControl = ctrl)
-## test model (predict test data)
-p <- predict(logistic_model, newdata = test_data)
-accuracy <- mean(p == test_data$diabetes)
+ctrl <- trainControl(method = "LOOCV") ## Leave one out CV
+
+lm_model <- train(mpg ~ hp,
+                  data = train_data,
+                  method = "lm",       ## Linear regression
+                  trControl = ctrl
+                  )
