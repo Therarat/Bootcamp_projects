776dd89bedf60767b640ffb62be78ee6904dc839
Therarat <117662533+Therarat@users.noreply.github.com>
2022-11-08 22:12:51 +0700

    Update Logistic_regression.r

--- Machine_learning/Logistic_regression.r
+++ Machine_learning/Logistic_regression.r
@@ -1,25 +1,10 @@
 set.seed(99)
-ctrl <- trainControl(method = "repeatedcv",
-                     number = 5,
-                     repeats = 5,
-                     verboseIter = T)
-
-grid <- data.frame(k = c(3, 5, 7))
-
-knn_model <- train(
-              medv ~ crim + indus + rm,
-              data = train_data,
-              method = "knn",
-              metric = "RMSE",   # Search for smallest RMSE
-              trControl = ctrl,
-              tuneGrid = grid
-              )
-
-plot(knn_model)
-
-#Score + evaluate
-
-p <- predict(knn_model,
-             newdata = test_data)
-
-RMSE(p, test_data$medv)
+ctrl <- trainControl(method = “cv”, number = 5, 
+ verboseIter = TRUE)
+logistic_model <- train(diabetes ~ ., 
+ data = train_data, 
+ method = "glm",
+ trControl = ctrl)
+## test model (predict test data)
+p <- predict(logistic_model, newdata = test_data)
+accuracy <- mean(p == test_data$diabetes)
